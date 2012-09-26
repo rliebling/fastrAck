3acbbc1a1f469a859928680dcc2c1a2cccd56676
rich <rich@shopittome.com>
2012-09-26 16:33:33 -0700

    Integrate index and search capabilities into same executable

--- searcher.go
+++ searcher.go
@@ -3,23 +3,171 @@ package main
 import (
   //"github.com/howeyc/fsnotify"
 	"github.com/rliebling/codesearch/index"
+	"github.com/rliebling/codesearch/regexp"
   "github.com/rliebling/fastrAck/dir_watcher"
 	"path/filepath"
 	"flag"
   "fmt"
 	"io"
+	"log"
   "os"
+	"sort"
 //	"strings"
 	"time"
 )
 
 type fileset map[string]bool
 
+var (
+	fileFilterFlag = flag.String("f", "", "search only files with names matching this regexp")
+	fileExclusionFlag = flag.String("F", "", "search excluding files with names matching this regexp")
+	iFlag       = flag.Bool("i", false, "case-insensitive search")
+	listFlag    = flag.Bool("list", false, "list indexed paths and exit")
+	indexFlag   = flag.Bool("index", false, "create index")
+	watchFlag   = flag.Bool("watch", false, "watch for changes")
+	indexFilename = flag.String("file", "./.cindex", "index filename")
+	verboseFlag = flag.Bool("verbose", false, "print extra information")
+	cpuProfile  = flag.String("cpuprofile", "", "write cpu profile to this file")
+)
+
 func main() {
 	flag.Parse()
+	if *watchFlag {
+		watch(".")
+	} else if *indexFlag {
+		createIndex(".")
+	} else {
+		search(flag.Args()...)
+	}
+}
+
+func search(args... string) {
+	g := regexp.Grep{
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	g.AddFlags()
+
+	pat := "(?m)" + args[0]
+	if *iFlag {
+		pat = "(?i)" + pat
+	}
+	re, err := regexp.Compile(pat)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.Regexp = re
+	var fre, fexclusion_re *regexp.Regexp
+	if *fileFilterFlag != "" {
+		fre, err = regexp.Compile(*fileFilterFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if *fileExclusionFlag != "" {
+		fexclusion_re, err = regexp.Compile(*fileExclusionFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	q := index.RegexpQuery(re.Syntax)
+	if *verboseFlag {
+		log.Printf("query: %s\n", q)
+	}
+
+	ix := index.Open(*indexFilename)
+	ix.Verbose = *verboseFlag
+	var post []uint32
+	post = ix.PostingQuery(q)
+
+	if *verboseFlag {
+		log.Printf("post query identified %d possible files\n", len(post))
+	}
+
+	if fre != nil || fexclusion_re != nil {
+		fnames := make([]uint32, 0, len(post))
+
+		for _, fileid := range post {
+			name := ix.Name(fileid)
+			if fre!=nil && fre.MatchString(name, true, true) < 0 {
+				continue
+			}
+			if fexclusion_re!=nil && fexclusion_re.MatchString(name, true, true) >= 0 {
+				continue
+			}
+			fnames = append(fnames, fileid)
+		}
+
+		if *verboseFlag {
+			log.Printf("filename regexp matched %d files\n", len(fnames))
+		}
+		post = fnames
+	}
+
+	for _, fileid := range post {
+		name := ix.Name(fileid)
+		g.File(name)
+	}
+
+	//matches = g.Match
+}
+
+func preparePaths(args []string) []string {
+	for i, arg := range args {
+		a, err := filepath.Abs(arg)
+		if err != nil {
+			log.Printf("%s: %s", arg, err)
+			args[i] = ""
+			continue
+		}
+		args[i] = a
+	}
+	sort.Strings(args)
+
+	for len(args) > 0 && args[0] == "" {
+		args = args[1:]
+	}
+	return args
+}
+
+func createIndex(args... string) {
+	dirs_to_index := preparePaths(args)
+	ix := index.Create(*indexFilename)
+	ix.Verbose = *verboseFlag
+	ix.AddPaths(dirs_to_index)
+	for _, arg := range dirs_to_index {
+		log.Printf("index %s", arg)
+		filepath.Walk(arg, func(path string, info os.FileInfo, err error) error {
+			if _, elem := filepath.Split(path); elem != "" {
+				// Skip various temporary or "hidden" files or directories.
+				if elem[0] == '.' || elem[0] == '#' || elem[0] == '~' || elem[len(elem)-1] == '~' {
+					if info.IsDir() {
+						return filepath.SkipDir
+					}
+					return nil
+				}
+			}
+			if err != nil {
+				log.Printf("%s: %s", path, err)
+				return nil
+			}
+			if info != nil && info.Mode()&os.ModeType == 0 {
+				ix.AddFile(path)
+			}
+			return nil
+		})
+	}
+	log.Printf("flush index")
+	ix.Flush()
+
+	log.Printf("done")
+	return
+}
+
+func watch(args... string) {
 	curdir,_ := filepath.Abs(".")
-  fmt.Printf("Hello %v %v\n", flag.Args(), curdir)
-	watcher, err := dir_watcher.Watch(flag.Args(), func(dirname string)bool {
+  fmt.Printf("Hello %v %v\n", args, curdir)
+	watcher, err := dir_watcher.Watch(args, func(dirname string)bool {
 		_, elem := filepath.Split(dirname)
 		// Skip various temporary or "hidden" files or directories.
 		return !(elem[0] == '.' || elem[0] == '#' || elem[0] == '~' || elem[len(elem)-1] == '~')
@@ -44,7 +192,7 @@ func main() {
 					continue
 				}
 
-				if !ev.IsDelete() {
+				if !skip_reindex(ev.Name) {
 					//fmt.Printf("EVENT: %s %#v\n" , ev.Name ,  ev)
 					//name := strings.Replace(ev.Name, "/", "\\", -1)
 					to_reindex[ev.Name] = true
@@ -75,13 +223,30 @@ func main() {
 	}
 }
 
+func skip_reindex(name string) bool {
+	if _, elem := filepath.Split(name); elem != "" {
+		// Skip various temporary or "hidden" files or directories.
+		if elem[0] == '.' || elem[0] == '#' || elem[0] == '~' || elem[len(elem)-1] == '~' {
+			fmt.Println("Skipping " + name)
+			return true
+		}
+		if _,err := os.Stat(name); err!=nil {
+			fmt.Println("Skipping (err stat'ing )" + name)
+			return true
+		}
+	}
+	return false
+}
+
 func reindex(paths []string, curdir string) {
-	fmt.Printf("Reindexing %v\n", paths)
-	master := index.File()
+	log.Printf("Reindexing %v\n", paths)
+	master := *indexFilename
+	log.Println("Master is ", master)
 	file := master + "~"
 	ix := index.Create(file)
 	//ix.AddPaths([]string{curdir})
 	//ix.AddPaths([]string{"c:\\Users\\rich\\workspace\\sitm"})
+	added_files := false
 	for _, p := range paths {
 		if _, elem := filepath.Split(p); elem != "" {
 			// Skip various temporary or "hidden" files or directories.
@@ -94,19 +259,29 @@ func reindex(paths []string, curdir string) {
 				continue
 			}
 		}
-		//fmt.Println("AddFile " , p)
+		fmt.Println("AddFile " , p)
 		ix.AddPaths([]string{p})
 		ix.AddFile(p)
+		added_files = true
 	}
 	ix.Flush()
 	ix.Close()
+
+	if !added_files {
+		os.Remove(file)
+		return
+	}
 
 	index.Merge(file+"~", master, file)
 	_, err := copyFile(master, file+"~")
 	if err != nil {
 		panic("copy: " + err.Error())
 	}
-	//err = os.Remove(file)
+	err = os.Remove(file)
+	if err != nil {
+		panic("Remove: " + err.Error())
+	}
+	err = os.Remove(file+"~")
 	if err != nil {
 		panic("Remove: " + err.Error())
 	}
