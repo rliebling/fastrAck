62253de50512deeaf662472cda1347548c328822
rich <rich@scientificrevenue.com>
2014-08-23 13:47:35 -0700

    index_cmd: remove file list filter in favor of using user's gitignore
    first step to a more robust usage of gitignore filtering
    more to come

--- index_cmd.go
+++ index_cmd.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/rliebling/codesearch/index"
 	"log"
 	"os"
 	"path/filepath"
 	"sort"
+
+	"github.com/rliebling/codesearch/index"
+	"github.com/rliebling/gitignorer"
 )
 
 func preparePaths(args []string) []string {
@@ -28,26 +30,23 @@ func preparePaths(args []string) []string {
 
 func createIndex(args ...string) {
 	dirs_to_index := preparePaths(args)
+	filter, _ := gitignorer.NewFilter()
 	ix := index.Create(*indexFilename)
 	ix.Verbose = *verboseFlag
 	ix.AddPaths(dirs_to_index)
 	for _, arg := range dirs_to_index {
 		log.Printf("index %s", arg)
 		filepath.Walk(arg, func(path string, info os.FileInfo, err error) error {
-			if _, elem := filepath.Split(path); elem != "" {
-				// Skip various temporary or "hidden" files or directories.
-				if elem[0] == '.' || elem[0] == '#' || elem[0] == '~' || elem[len(elem)-1] == '~' {
-					if info.IsDir() {
-						return filepath.SkipDir
-					}
-					return nil
-				}
-			}
 			if err != nil {
 				log.Printf("%s: %s", path, err)
 				return nil
 			}
-			if info != nil && info.Mode()&os.ModeType == 0 {
+
+			if filter.Match(path) {
+				return filepath.SkipDir
+			}
+
+			if info != nil && isRegularFile(info) {
 				ix.AddFile(path)
 			}
 			return nil
@@ -59,4 +58,8 @@ func createIndex(args ...string) {
 
 	log.Printf("done")
 	return
+}
+
+func isRegularFile(info os.FileInfo) bool {
+	return info.Mode()&os.ModeType == 0
 }
